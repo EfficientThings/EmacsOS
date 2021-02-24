31389fcce52b7aa207d9f699172296cd5ad2b245
David Freifeld <freifeld.david@gmail.com>
2021-02-24 13:30:22 -0800

    More flexible build option prompt

--- install.sh
+++ install.sh
@@ -10,7 +10,6 @@ function printf_bad()
     printf "\u001b[31m"$1"\u001b[0m"
 }
 
-
 function brew_ensure()
 {
     printf "Checking for $1 dependency... "
@@ -63,85 +62,85 @@ echo "Checking out native-comp branch..."
 #git checkout features/native-comp &> /dev/null
 printf "\u001b[32mDone checking out native-comp!\u001b[0m\n"
 
-printf "Determining OS... "
-if [ $(uname -s) == "Darwin" ]; then
-    OS="Darwin"
-    printf "Darwin.\n"
-    printf "Checking for brew... "
-    if ! which brew &> /dev/null; then
-        printf "\u001b[31mnot installed.\u001b[0m\n"
-        exit 1
-    else
-        printf_good "installed.\n"
-    fi
+# printf "Determining OS... "
+# if [ $(uname -s) == "Darwin" ]; then
+#     OS="Darwin"
+#     printf "Darwin.\n"
+#     printf "Checking for brew... "
+#     if ! which brew &> /dev/null; then
+#         printf "\u001b[31mnot installed.\u001b[0m\n"
+#         exit 1
+#     else
+#         printf_good "installed.\n"
+#     fi
 
-    brew_ensure gcc@10
-    brew_ensure libgccjit
-    brew_ensure llvm
-    brew_ensure jpeg
-    brew_ensure libtiff
-    brew_ensure gnutls
-    brew_ensure nettle
-    brew_ensure libtasn1
-    brew_ensure p11-kit
+#     brew_ensure gcc@10
+#     brew_ensure libgccjit
+#     brew_ensure llvm
+#     brew_ensure jpeg
+#     brew_ensure libtiff
+#     brew_ensure gnutls
+#     brew_ensure nettle
+#     brew_ensure libtasn1
+#     brew_ensure p11-kit
 
-    libs=(
-        /usr/local/Cellar/gcc/10.2.0_4
-        /usr/local/Cellar/giflib/5.2.1
-        /usr/local/Cellar/jpeg/9d
-        /usr/local/Cellar/libtiff/4.2.0
-        /usr/local/Cellar/gnutls/3.6.15
-        /usr/local/Cellar/nettle/3.7
-        /usr/local/Cellar/libtasn1/4.16.0
-        /usr/local/Cellar/p11-kit/0.23.22
-    )
-    export CFLAGS="-I/usr/local/Cellar/gcc/10.2.0_4/include -O2 -march=native"
-    export LDFLAGS="-L/usr/local/Cellar/gcc/10.2.0_4/lib/gcc/10 -I/usr/local/Cellar/gcc/10.2.0_4/include"
-    export PATH="/usr/local/Cellar/gcc/:${PATH}"
-    export LIBRARY_PATH="/usr/local/Cellar/gcc/10.2.0_4/lib/gcc/10:${LIBRARY_PATH:-}"
-else
-    OS="Linux"
-    printf "Linux (assumed to be Arch).\n"
-    pacman_ensure gcc
-    printf "Checking gcc version... "
-    if [ $(gcc --version | grep ^gcc | sed 's/^.* //g') == "10.2.0" ]; then
-        printf_good "good.\n"
-    else
-        printf_bad "bad!\n"
-        exit 1
-    fi
-    pacman_ensure libgccjit
-    printf "Checking that /usr/lib/libjpeg.so exists... "
-    if [ -f "/usr/lib/libjpeg.so" ]; then
-        printf_good "yup.\n"
-    else
-        printf_bad "nope!\n"
-        exit 1
-    fi
-    pacman_ensure clang
-    pacman_ensure libtiff
-    pacman_ensure gnutls
-    pacman_ensure nettle
-    pacman_ensure libtasn1
-    pacman_ensure p11-kit
+#     libs=(
+#         /usr/local/Cellar/gcc/10.2.0_4
+#         /usr/local/Cellar/giflib/5.2.1
+#         /usr/local/Cellar/jpeg/9d
+#         /usr/local/Cellar/libtiff/4.2.0
+#         /usr/local/Cellar/gnutls/3.6.15
+#         /usr/local/Cellar/nettle/3.7
+#         /usr/local/Cellar/libtasn1/4.16.0
+#         /usr/local/Cellar/p11-kit/0.23.22
+#     )
+#     export CFLAGS="-I/usr/local/Cellar/gcc/10.2.0_4/include -O2 -march=native"
+#     export LDFLAGS="-L/usr/local/Cellar/gcc/10.2.0_4/lib/gcc/10 -I/usr/local/Cellar/gcc/10.2.0_4/include"
+#     export PATH="/usr/local/Cellar/gcc/:${PATH}"
+#     export LIBRARY_PATH="/usr/local/Cellar/gcc/10.2.0_4/lib/gcc/10:${LIBRARY_PATH:-}"
+# else
+#     OS="Linux"
+#     printf "Linux (assumed to be Arch).\n"
+#     pacman_ensure gcc
+#     printf "Checking gcc version... "
+#     if [ $(gcc --version | grep ^gcc | sed 's/^.* //g') == "10.2.0" ]; then
+#         printf_good "good.\n"
+#     else
+#         printf_bad "bad!\n"
+#         exit 1
+#     fi
+#     pacman_ensure libgccjit
+#     printf "Checking that /usr/lib/libjpeg.so exists... "
+#     if [ -f "/usr/lib/libjpeg.so" ]; then
+#         printf_good "yup.\n"
+#     else
+#         printf_bad "nope!\n"
+#         exit 1
+#     fi
+#     pacman_ensure clang
+#     pacman_ensure libtiff
+#     pacman_ensure gnutls
+#     pacman_ensure nettle
+#     pacman_ensure libtasn1
+#     pacman_ensure p11-kit
 
-    libs=(
-        /usr/lib/gcc/x86_64-pc-linux-gnu/10.2.0_4
-        /usr/lib/
-        /usr/lib/p11-kit/
-    )
-    export CFLAGS="-I/usr/lib/gcc/x86_64-pc-linux-gnu/10.2.0/include -O2 -march=native"
-    export LDFLAGS="-L/usr/lib/"
-    export PATH="/usr/lib/gcc/x86_64-pc-linux-gnu/10.2.0:${PATH}"
-    export LIBRARY_PATH="/usr/lib:${LIBRARY_PATH:-}"
-fi
+#     libs=(
+#         /usr/lib/gcc/x86_64-pc-linux-gnu/10.2.0_4
+#         /usr/lib/
+#         /usr/lib/p11-kit/
+#     )
+#     export CFLAGS="-I/usr/lib/gcc/x86_64-pc-linux-gnu/10.2.0/include -O2 -march=native"
+#     export LDFLAGS="-L/usr/lib/"
+#     export PATH="/usr/lib/gcc/x86_64-pc-linux-gnu/10.2.0:${PATH}"
+#     export LIBRARY_PATH="/usr/lib:${LIBRARY_PATH:-}"
+# fi
 
-PKG_CONFIG_PATH=""
+# PKG_CONFIG_PATH=""
 
-export CPPFLAGS="${CFLAGS}"
-export CFLAGS
-export LDFLAGS
-export PKG_CONFIG_PATH
+# export CPPFLAGS="${CFLAGS}"
+# export CFLAGS
+# export LDFLAGS
+# export PKG_CONFIG_PATH
 
 
 ./autogen.sh
@@ -154,53 +153,47 @@ elif [[ $SEL_CC == "gcc" ]]; then
     export CC="gcc"
 fi
 
+QUESTIONS=("Disable silent rules? (y/n/?)" "Enable native compilation? (y/n/?)" "Enable dbus support? (y/n/?)" "Enable json support? (y/n/?)" "Use Cairo instead of ImageMagick? (y/n/?)")
+EXPECTED_ANSWERS=("y" "y" "n" "y" "y")
+DOCUMENTATION=(
+"Use --disable-silent-rules to cause 'make' to give more details about the commands it executes.
+This can be helpful when debugging a build that goes awry. 'make V=1' also enables the extra chatter."
+
+"gccemacs (or native-comp) is a modified Emacs capable of compiling and running Emacs Lisp as native
+code in form of re-loadable elf files. As the name suggests this is achieved by blending together Emacs
+and the gcc infrastructure."
+
+"Emacs will autolaunch a D-Bus session bus, when the environment variable DISPLAY is set, but no
+session bus is running. This might be inconvenient for Emacs when running as daemon or running via a
+remote ssh connection."
+
+"Emacs 27 brought JSON support."
+
+"Cairo is a 2D graphics library with support for multiple output devices. Cairo is designed to produce
+consistent output on all output media while taking advantage of display hardware acceleration when
+available (eg. through the X Render Extension)."
+)
+FLAGS=("--disable-silent-rules" "--with-nativecomp" "--without-dbus --without-gconf --without-gsettings" "--with-json" "--with-cairo --without-imagemagick")
+
 CONFIGURE_COMMAND="./configure"
-read -p "Disable silent rules? (y/n) " ANS 
-if [[ $ANS == "y" || $ANS == "Y" ]]; then
-    CONFIGURE_COMMAND+=" --disable-silent-rules"
-fi
-read -p "Enable native compilation? (y/n) " ANS 
-if [[ $ANS == "y" || $ANS == "Y" ]]; then
-    CONFIGURE_COMMAND+=" --with-nativecomp"
-fi
-read -p "Enable dbus support? (y/n) " ANS 
-if [[ $ANS == "n" || $ANS == "N" ]]; then
-    CONFIGURE_COMMAND+=" --without-dbus"
-fi
-read -p "Enable json support? (y/n) " ANS 
-if [[ $ANS == "y" || $ANS == "y" ]]; then
-    CONFIGURE_COMMAND+=" --with-json"
-fi
-read -p "Use Cairo instead of ImageMagick? (y/n) " ANS 
-if [[ $ANS == "y" || $ANS == "y" ]]; then
-    CONFIGURE_COMMAND+=" --without-imagemagick --with-cairo"
-fi
-read -p "Enable mail utilities? (y/n) " ANS 
-if [[ $ANS == "y" || $ANS == "y" ]]; then
-    CONFIGURE_COMMAND+=" --with-mailutils"
-fi
-read -p "Enable xml2? (y/n) " ANS 
-if [[ $ANS == "y" || $ANS == "y" ]]; then
-    CONFIGURE_COMMAND+=" --with-xml2"
-fi
-read -p "Enable gnutls? (y/n) " ANS 
-if [[ $ANS == "y" || $ANS == "y" ]]; then
-    CONFIGURE_COMMAND+=" --with-gnutls"
-fi
-read -p "Enable modules? (y/n) " ANS 
-if [[ $ANS == "y" || $ANS == "y" ]]; then
-    CONFIGURE_COMMAND+=" --with-modules"
-fi
-read -p "Enable rsvg? (y/n) " ANS 
-if [[ $ANS == "y" || $ANS == "y" ]]; then
-    CONFIGURE_COMMAND+=" --with-modules"
-fi
+
+for (( i=0; i<${#QUESTIONS[@]}; i++ )); do
+    read -p "${QUESTIONS[$i]} " ANS
+    if [[ $ANS == $EXPECTED_ANSWERS[$i] ]]; then
+        CONFIGURE_COMMAND+=" ${FLAGS[$i]}"
+    elif [[ $ANS == "?" ]]; then
+        echo "${DOCUMENTATION[$i]} "
+        i=$i-1
+    fi
+done
+
+
 if [ $OS == "Darwin" ]; then
     CONFIGURE_COMMAND+=" --with-ns --with-cocoa"
 fi
 
 $CONFIGURE_COMMAND
-    
+
 make
 # cd ..
 
